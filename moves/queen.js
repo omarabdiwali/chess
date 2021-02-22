bbadd3afa507e7009c18c3da66a3b18ba59dde04
omarabdiwali <63807504+omarabdiwali@users.noreply.github.com>
2021-02-22 13:37:07 -0600

    Update queen.js

--- moves/queen.js
+++ moves/queen.js
@@ -1,12 +1,6 @@
 function queen(col, pos) {
   let a = rook(col, pos);
-  let b;
-  if (col == "w") {
-    b = bishop("b", pos);
-  }
-  else {
-    b = bishop("w", pos);
-  } 
+  let b = col == "w" ? bishop("b", pos) : bishop("w", pos);
   let valid = a.concat(b.filter((item) => a.indexOf(item) < 0));
   return valid;
 }
